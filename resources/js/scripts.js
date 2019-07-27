54ab2a893053b908dc986bb4cde15ad76ca5af77
badnguyenky <52956049+badnguyenky@users.noreply.github.com>
2019-07-27 17:57:13 +0700

    Add files via upload

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -20,3 +20,36 @@ $("a[href*='#']:not([href='#])").click(function() {
     }, 1000);
     event.preventDefault();
 });
+
+// Typewritter Effects
+var pos = 0;
+var turn = 0;
+var data = ['Skateboarding saves my life'];
+var speed = 200;
+
+setTimeout(typeWriter, speed);
+
+function typeWriter() {
+  if (pos < data[turn].length) {
+    document.getElementById("slogan").innerHTML += data[turn].charAt(pos);
+    console.log(data[turn].charAt(pos));
+    pos++;
+    setTimeout(typeWriter, speed);
+  } else {
+  	setTimeout(erase, speed+200);
+  }
+}
+
+function erase() {
+	if (pos >= 0) {
+      var str=data[turn].toString().substring(0, pos);
+      document.getElementById("slogan").innerHTML = str;
+      pos--;
+      setTimeout(erase, speed-100);
+    } else {
+      turn++;
+      if(turn>=data.length) 
+        turn=0;
+      setTimeout(typeWriter, speed);
+    }
+}
